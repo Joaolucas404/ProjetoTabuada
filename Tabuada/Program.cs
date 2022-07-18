411089c168e883616f023d08013690b3d36bb5ff
joaolucas404 <joaolucasssa21@gmail.com>
2022-07-18 10:20:53 -0300

    Lista 4

---
+++ Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Digite um número para fazer a tabuada");
+            numero = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for(int x = 0; x <= 10; x++)
+            {
+                resultado = numero * x;
+                Console.WriteLine(x+" x "+numero+" = "+resultado);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
